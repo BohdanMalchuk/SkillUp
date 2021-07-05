bf166ab9ee0f7df1a7f861bf8f37a4a9d04add33
Your Name <bohdan043@gmail.com>
2021-07-05 13:04:19 +0300

    Lesson_2_1

---
+++ Lesson_2_1.py
@@ -0,0 +1,89 @@
+#print
+print(abs(2))
+print(abs(-6))
+print(abs(200))
+
+#ord
+print(ord('a'))
+print(ord('b'))
+print(ord('&'))
+print(ord('h'))
+
+#pow
+print(pow(2,3))
+print(pow(38,-1,mod=97))
+print(pow(5,5))
+print(pow(4,2))
+print(pow(4,2,mod=2))
+
+#str
+str1 = "I"
+str2 = "Love"
+str3 = "Python"
+print(str1)
+print(str2)
+print(str3)
+print(str1 + str2 + str3)
+
+#ascii
+print(ascii('телефон'))
+print(ascii('окно'))
+print(ascii('кровать'))
+
+#bin
+print(bin(3))
+print(bin(-10))
+print(bin(235))
+
+#bool
+my_age = 26
+ 
+if my_age >= 100:
+    print("Сто лет! Впечатляет.")
+elif my_age <= 3:
+  print("Оуу. Совсем малыш.")
+else:
+  print("Эх, какой хороший возраст.")
+
+I_love = "bike"
+
+if not I_love == "bike":
+    print("car is suck")
+else:
+    print("Fuck yea")
+
+#int
+print(int(10))
+print(int(10.5))
+print(int(185.999))
+
+#float
+print(float(10))
+print(float('10'))
+print(float(10.6))
+print(float(100000))
+
+#print
+print("I love summer")
+print("{}")
+
+#format
+print("Hello {0}, your balance is {1}".format("Adam",230))
+print("i work in {0}, and i'm {1}".format("google","happy"))
+
+#chr
+print(chr(8364))
+print(chr(97))
+
+#complex
+x = complex (1, 2)
+print(x)
+y = complex (5, 7)
+print(y)
+z = x * y
+print(z)
+
+#round
+print(round(1.5))
+print(round(9.9))
+
