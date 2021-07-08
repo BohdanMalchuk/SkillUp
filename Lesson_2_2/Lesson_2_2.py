d3d2e9c4a6ce9988721cbe84a0d436fb23680243
Your Name <bohdan043@gmail.com>
2021-07-08 19:09:09 +0300

    Lesson_2_2(fixed)

--- Lesson_2_2/Lesson_2_2.py
+++ Lesson_2_2/Lesson_2_2.py
@@ -4,25 +4,25 @@ b=4
 c=6
 d=a+b+c
 e=a*b*c
-print("Cумма =", d)
-print("Произведение =", e)
+print('Cумма =', d)
+print('Произведение =', e)
 #
 a=float(input( ))
 b=float(input( ))
 c=float(input( ))
 d=a+b+c
 e=a*b*c
-print("Сумма =", d)
-print("Произвидение =", e)
+print('Сумма =', d)
+print('Произвидение =', e)
 
 
 # # Пользователь вводит Имя и Фамилию. Нужно вывести приветствие для пользователя в новой системе.
-name = input("Enter your first name: ") + " " + input("Enter your surname: ")
-print("Hello, dear", name)
+name = input('Enter your first name: ') + " " + input('Enter your surname: ')
+print('Hello, dear', name)
 
 # # Пользователь вводит количество метров. Нужно конвертировать данные в километры и миллиметры.
-metr=float(input("Метры"))
+metr=float(input('Метры'))
 km=metr/1000
 mm=metr*1000
-print("Kilometr =", km)
-print("Milimetr =", mm)
+print('Kilometr =', km)
+print('Milimetr =', mm)
